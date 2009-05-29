4280140d2dfbac5b62b91f7122cb42a1bcde1bc8
William Roberts <williamr@symbian.org>
2009-05-29 14:13:23 +0100

    Add EPL license text to the synch_hg* scripts

--- synch_hg_p4/synch_hg_p4.pl
+++ synch_hg_p4/synch_hg_p4.pl
@@ -1,6 +1,18 @@
 #! perl
 
-# sync_hg_p4.pl
+# Copyright (c) 2009 Symbian Foundation Ltd
+# This component and the accompanying materials are made available
+# under the terms of the License "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Symbian Foundation Ltd - initial contribution.
+# 
+# Contributors:
+#
+# Description:
+# Perl script to synchronise a Perforce branch with a Mercurial repository
 
 use strict;
 use Getopt::Long;
