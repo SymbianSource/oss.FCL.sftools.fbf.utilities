5bfcbfcada4e51f2028802c42c70ca59c6f24fd6
MattD <mattd@symbian.org>
2010-03-11 12:35:33 +0000

    wikify_bom_fcl_changes.pl - Added some basic tags displaying.

--- releaseAutomation/wikify_bom_fcl_changes.pl
+++ releaseAutomation/wikify_bom_fcl_changes.pl
@@ -22,6 +22,7 @@ open(FILE, "<$file") or die "Coudln't open $file\n";
 my $fcl = undef;
 my $changeset = undef;
 my $user = undef;
+my $tag = "";
 while(my $line = <FILE>)
 {
   if($line =~ m/(\S+)(\/FCL\/\S+)/i)
@@ -29,6 +30,8 @@ while(my $line = <FILE>)
     my $codeline = $1;
     my $location = $2;
     my $root;
+    $tag = "";
+
     if ($codeline =~ m/oss/i)
     {
       $root = "http://developer.symbian.org/oss" 
@@ -70,6 +73,18 @@ while(my $line = <FILE>)
     #changeset:   118:c5817fd289ec
     $user = $1;
   }
+  elsif($line =~ m/^tag:\s+(\S+)/)
+  {
+    #changeset:   118:c5817fd289ec
+    my $preprocessed = $1;
+    $preprocessed =~ s/^tip$//g;
+    if($preprocessed =~ m/\S+/)
+    {
+      $tag = $tag."\'\'\'".$preprocessed."\'\'\' ";
+    }  
+    
+#    $tag = $1." ";
+  }
   elsif( defined $fcl)
   {
     if($line =~ s/^summary:\s+//)
@@ -77,7 +92,7 @@ while(my $line = <FILE>)
       $line =~ s/\n//;
       my $bugzilla = "http:\/\/developer.symbian.org\/bugs\/show_bug.cgi?id=";
       $line =~ s/(bug\s*)(\d+)/\[$bugzilla$2 $1$2\]/gi;
-      print "|[".$fcl."rev\/".$changeset." ".$changeset."]\n|".$line."\n|-\n";
+      print "|[".$fcl."rev\/".$changeset." ".$changeset."]\n|".$tag.$line."\n|-\n";
 #      print "|[".$fcl."rev\/".$changeset." ".$changeset."]\n|".$user."\n|".$line."\n|-\n";
     }
     #abort: unknown revision 'PDK_3.0.c'!
