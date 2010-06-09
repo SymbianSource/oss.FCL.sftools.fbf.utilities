d573c0c06e5a0778b524ce431ee22bb442b8f405
Dario Sestito <darios@symbian.org>
2010-06-09 18:41:17 +0100

    Add stubs for the most commonly used tools

---
+++ stubs/envinfo.bat
@@ -0,0 +1,24 @@
+@REM Copyright (c) 2009 Symbian Foundation Ltd
+@REM This component and the accompanying materials are made available
+@REM under the terms of the License "Eclipse Public License v1.0"
+@REM which accompanies this distribution, and is available
+@REM at the URL "http://www.eclipse.org/legal/epl-v10.html".
+@REM
+@REM Initial Contributors:
+@REM Symbian Foundation Ltd - initial contribution.
+@REM
+@REM Contributors:
+@REM
+@REM Description:
+@REM Invokes the tool by call to the related interpreter
+
+@SETLOCAL
+@IF NOT "%PDT_HOME%"=="" (
+SET UTILITIES_HOME=%PDT_HOME%\utilities
+) ELSE (
+SET UTILITIES_HOME=%~dp0\..
+)
+
+@perl %UTILITIES_HOME%\envinfo\envinfo.pl %*
+
+@ENDLOCAL
